74148c9891b43244036c88aef872c963d0d56986
De <spark49942@gmail.com>
2022-09-06 17:39:04 +0300

    Alpha vers-2

--- Program.cs
+++ Program.cs
@@ -1,26 +1,29 @@
 ﻿int si = 0;
+string [] ray = new string [si];
 int answ = 1;
-Console.Write("Введите строку: ");
+Console.WriteLine("Введите строку: ");
+Console.Write("[");
 bool ent = true;
 while (ent == true) 
 {
     int i = 0;
     string s = Convert.ToString(Console.ReadLine());
+    ray[si] = s;
+    Console.Write(ray[i] + ", ");
     si++;
     if (i==4) 
         { 
             Console.WriteLine("Продолжить? 1/0");
             answ = Convert.ToInt32(Console.ReadLine());
-            if (answ==0) { ent = false; }
+            if (answ==0) { ent = false; Console.Write("\b\b]");}
             else if (answ==1) { ent = true; }
             else Console.WriteLine("Введено некорректное значение.");
         }
 }
-string [] ray = new string [si];
 //string [] ray = {"hello", "2", "world", ":-("};
 Console.Write("[");
 for(int i = 0; i<ray.Length; i++)
 {
     if (ray[i].Length<=3) { Console.Write(ray[i] + ", "); }
 }
-Console.Write("\b\b]");
+ Console.Write("\b\b]");
