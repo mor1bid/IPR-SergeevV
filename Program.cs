c7ad7969c7ec384b32ad88434b5cf291ed02ab42
De <spark49942@gmail.com>
2022-09-06 18:12:38 +0300

    WTH vers

--- Program.cs
+++ Program.cs
@@ -1,28 +1,31 @@
-﻿int si = 0;
+﻿int i = 0;
+int si = 4;
+int ari = 0;
 string [] ray = new string [si];
-int answ = 1;
 Console.WriteLine("Введите строку: ");
 Console.Write("[");
 bool ent = true;
 while (ent == true) 
 {
-    int i = 0;
-    string s = Convert.ToString(Console.ReadLine());
-    ray[si] = s;
+    string s = Convert.ToString(Console.Read());
+    ray[ari] = s;
     Console.Write(ray[i] + ", ");
-    si++;
+    ari++;
+    i++;
     if (i==4) 
-        { 
-            Console.WriteLine("Продолжить? 1/0");
-            answ = Convert.ToInt32(Console.ReadLine());
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Продолжить?: 1/0");
+            int answ = Convert.ToInt32(Console.ReadLine());
             if (answ==0) { ent = false; Console.Write("\b\b]");}
-            else if (answ==1) { ent = true; }
+            else if (answ==1) { i = 0; si+=3;}
             else Console.WriteLine("Введено некорректное значение.");
         }
 }
 //string [] ray = {"hello", "2", "world", ":-("};
+Console.WriteLine("");
 Console.Write("[");
-for(int i = 0; i<ray.Length; i++)
+for(i = 0; i<ray.Length; i++)
 {
     if (ray[i].Length<=3) { Console.Write(ray[i] + ", "); }
 }
