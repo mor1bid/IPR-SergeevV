543127074f4f4f5f4371227b29d440e1d80da3de
De <spark49942@gmail.com>
2022-09-06 18:47:30 +0300

    Better version

--- Program.cs
+++ Program.cs
@@ -1,26 +1,33 @@
 ﻿int i = 0;
-int si = 4;
+int si = 999;
+int ze = 0;
 int ari = 0;
-string [] ray = new string [si];
+string [] ar = new string [si];
 Console.WriteLine("Введите строку: ");
 Console.Write("[");
 bool ent = true;
 while (ent == true) 
 {
-    string s = Convert.ToString(Console.Read());
-    ray[ari] = s;
-    Console.Write(ray[i] + ", ");
+    string s = Convert.ToString(Console.ReadLine());
+    ar[ari] = s;
+    //Console.Write(ar[i] + ", ");
     ari++;
+    ze++;
     i++;
     if (i==4) 
         {
-            Console.WriteLine("");
+            Console.WriteLine("\b\b]");
             Console.WriteLine("Продолжить?: 1/0");
             int answ = Convert.ToInt32(Console.ReadLine());
-            if (answ==0) { ent = false; Console.Write("\b\b]");}
-            else if (answ==1) { i = 0; si+=3;}
+            if (answ==0) { ent = false;}
+            else if (answ==1) { i = 0; Console.WriteLine("[");}
             else Console.WriteLine("Введено некорректное значение.");
         }
+}
+string [] ray = new string [ze];
+for(i = 0; i<ze; i++)
+{
+    ray[i] = ar[i];
 }
 //string [] ray = {"hello", "2", "world", ":-("};
 Console.WriteLine("");
