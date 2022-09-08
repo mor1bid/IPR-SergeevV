a369f7a76c330dfc2a020014bc4a82b5b5fcb657
De <spark49942@gmail.com>
2022-09-08 04:05:59 +0300

    Experiment-metodi

--- Program.cs
+++ Program.cs
@@ -6,22 +6,30 @@
     string [] ar = new string [si];
     Console.WriteLine("Введите строки (для получения результата нажмите Enter): ");
     Console.WriteLine("[");
-    bool ent = true;
-    while (ent == true) 
+    void ProcessAr() 
     {
+        bool ent = true;
+        while (ent == true) 
+        {
             string s = Convert.ToString(Console.ReadLine());
             ar[i] = s;
             string answ = "";
             ze++;
             i++;
             if (s==answ) {ent = false;}
+        }
     }
+    ProcessAr();
     Console.WriteLine("\b\b]");
     Console.Write("Строки, длина которых меньше/равна 3: [");
-    for(i = 0; i<ze; i++)
+    void BuildAr() 
     {
-        if (ar[i].Length<=3 && ar[i].Length!=0) { Console.Write(ar[i] + ", "); }
+        for(i = 0; i<ze; i++)
+        {
+            if (ar[i].Length<=3 && ar[i].Length!=0) { Console.Write(ar[i] + ", "); }
+        }
     }
+    BuildAr();
     Console.Write("\b\b]");
 }
 IPR();
