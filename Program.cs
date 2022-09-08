a786da61545143ae2108d567da32470f976c256d
De <spark49942@gmail.com>
2022-09-08 03:50:51 +0300

    Experiment

--- Program.cs
+++ Program.cs
@@ -3,45 +3,24 @@
     int i = 0;
     int si = 999;
     int ze = 0;
-    int ari = 0;
     string [] ar = new string [si];
-    Console.WriteLine("Введите 4 строки: ");
+    Console.WriteLine("Введите строки (для получения результата нажмите Enter): ");
     Console.WriteLine("[");
     bool ent = true;
     while (ent == true) 
     {
-        if (i<4) 
-        {
             string s = Convert.ToString(Console.ReadLine());
-            ar[ari] = s;
-            ari++;
+            ar[i] = s;
+            string answ = "";
             ze++;
             i++;
-        }
-        else if (i==4) 
-        {
-            Console.WriteLine("");
-            Console.WriteLine("Продолжить?: y/n");
-            string answ = Convert.ToString(Console.ReadLine());
-            string y = "y";
-            string n = "n";
-            if (answ==n) {ent = false;}
-            else if (answ==y) {i = 0; Console.WriteLine(""); Console.WriteLine("[");}
-            else {Console.WriteLine("Введено некорректное значение. Повторите попытку.");}
-        }
+            if (s==answ) {ent = false;}
     }
     Console.WriteLine("\b\b]");
-    string [] ray = new string [ze];
+    Console.Write("Строки, длина которых меньше/равна 3: [");
     for(i = 0; i<ze; i++)
     {
-        ray[i] = ar[i];
-    }
-    //string [] ray = {"hello", "2", "world", ":-("};
-    Console.WriteLine("");
-    Console.Write("Строки, длина которых меньше/равна 3: [");
-    for(i = 0; i<ray.Length; i++)
-    {
-        if (ray[i].Length<=3) { Console.Write(ray[i] + ", "); }
+        if (ar[i].Length<=3 && ar[i].Length!=0) { Console.Write(ar[i] + ", "); }
     }
     Console.Write("\b\b]");
 }
